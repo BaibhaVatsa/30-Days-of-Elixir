3ff36521cd6bc7786a8129b8d01a9f4307a3888f
Baibhav Vatsa <baibhavvatsa@gmail.com>
2019-11-16 01:44:38 -0600

    formatting and added to readme

--- 03_cli_calculator.ex
+++ 03_cli_calculator.ex
@@ -1,33 +1,33 @@
 # Objective: Accept user input and then if it is correct, print out the result. Else, throw error. Supported operations: +, -, *, /, ^, %. Supported complexity: op1 op op2
 
 defmodule Day03 do
-	def main do
-		expr = IO.gets("> ") |> String.trim() |> String.split()
+  def main do
+    expr = IO.gets("> ") |> String.trim() |> String.split()
 		
-		cond do
-			length(expr) == 3 -> expr
-			true -> IO.puts :stderr, "Error: Invalid expression"
-		end
-		|> parse_and_calculate()
-		|> IO.puts()
+    cond do
+      length(expr) == 3 -> expr
+      true -> IO.puts :stderr, "Error: Invalid expression"
+    end
+    |> parse_and_calculate()
+    |> IO.puts()
 		
-		main()
-	end
+    main()
+  end
 
-	def parse_and_calculate(expr_list) do
-		operator = Enum.at expr_list, 1
-	  operand_1 = Enum.at(expr_list, 0)|> String.to_integer()
-	  operand_2 = Enum.at(expr_list, 2)|> String.to_integer()	
+  def parse_and_calculate(expr_list) do
+    operator = Enum.at expr_list, 1
+    operand_1 = Enum.at(expr_list, 0)|> String.to_integer()
+    operand_2 = Enum.at(expr_list, 2)|> String.to_integer()	
 		
-		case operator do
-			"+" -> operand_1 + operand_2
-			"-" -> operand_1 - operand_2
-			"*" -> operand_1 * operand_2
-			"/" -> div operand_1, operand_2
-			"^" -> :math.pow(operand_1, operand_2) |> round()
-			"%" -> rem operand_1, operand_2
-		end	
-	end
+    case operator do
+      "+" -> operand_1 + operand_2
+      "-" -> operand_1 - operand_2
+      "*" -> operand_1 * operand_2
+      "/" -> div operand_1, operand_2
+      "^" -> :math.pow(operand_1, operand_2) |> round()
+      "%" -> rem operand_1, operand_2
+    end	
+  end
 end
 
 Day03.main()
--- README.md
+++ README.md
@@ -2,9 +2,11 @@
 
 Compilation of byte sized exercises to help your journey through Elixir
 
-> Progress: 2/30 (6.67%)
+> Progress: 3/30 (10%)
 
 [01 Hello World](./01_hello_world.ex) - Hello, World!
 
 [02 Fibonacci](./02_fibonacci.ex) - Fibonacci Sequence
+
+[03 Calculator](./03_cli_calculator.ex) - CommandLine Calculator
 
