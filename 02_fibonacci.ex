291671d6c3d471e637b53fe21f81031bb90f201d
Baibhav Vatsa <baibhavvatsa@gmail.com>
2020-01-14 04:10:18 -0600

    day 2 - recursive

--- 02_fibonacci.ex
+++ 02_fibonacci.ex
@@ -1,15 +1,10 @@
-# Output: Given n, prints the sequence for n terms
+# Output: Given n, returns the nth fibonacci number
 
 defmodule Day02 do
-	def fibonacci(n) do
-		case n do
-			_ when n <= 0 -> 0
-			1             -> 0
-			2             -> 1
-			_             -> fibonacci(n - 1) + fibonacci(n - 2)
-		end
-	end
+	def fibonacci(n) when n <= 0, do: raise "n must be > 0"
+	def fibonacci(1), do: 0
+	def fibonacci(2), do: 1
+	def fibonacci(n), do: fibonacci(n - 1) + fibonacci(n - 2)
 end
 
-IO.puts Day02.fibonacci(2)
-
+IO.puts Day02.fibonacci(14)
