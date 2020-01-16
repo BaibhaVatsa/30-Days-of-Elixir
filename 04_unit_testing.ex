50576fc41ad0290536e498f18b76be05c863cc95
Baibhav Vatsa <baibhavvatsa@gmail.com>
2020-01-16 14:05:24 -0600

    day 4 - unit testing complete

--- 04_unit_testing.ex
+++ 04_unit_testing.ex
@@ -1,6 +1,6 @@
 # Objective: Try out ExUnit for unit testing
 
-ExUnit.start()
+ExUnit.start
 
 defmodule Day04 do
 	use ExUnit.Case, async: true
@@ -11,15 +11,23 @@ defmodule Day04 do
 
     test "assert_raise", do: assert_raise ArithmeticError, fn -> 1 + "test" end
 
-    # test "assert_receive" do
-    
-    # test "assert_received" do
+    test "assert_receive" do
+        send self, :bye
+        assert_receive :bye, 0, "oh hi"
+    end
+
+    test "assert_received" do
+        send self, :bye
+        assert_received :hello, "guess it is a bye"
+    end
         
-    # test "catch_error" do
+    test "catch_error_throw", do: assert catch_error(throw 1) == 1
 
-    # test "catch_exit" do 
+    test "catch_error_raise", do: assert catch_error(raise "fails") == %RuntimeError{message: "fails"}
 
-    # test "catch_throw" do
+    test "catch_exit", do: assert catch_exit(exit 1) == 1 
+
+    test "catch_throw", do: assert catch_throw(throw 1) == 1
 
     test "flunk", do: flunk "failed successfully!"
 
@@ -27,7 +35,13 @@ defmodule Day04 do
 
     test "refute_delta", do: refute_in_delta 1.5, 2.5, 3.5, "2.5 - 1.5 == 3.5"
         
-    # test "refute_receive" do
+    test "refute_receive" do
+        send self, :hi
+        refute_receive :bye, 1000, "goodbye"
+    end
 
-    # test "refute_received" do
+    test "refute_received" do
+        send self, 56
+        refute_received 56, "received 56"
+    end
 end
